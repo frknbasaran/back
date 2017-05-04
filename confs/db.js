a6d8e042a948bce42f92ed3863f3e51c47a4bd48
Furkan Başaran <frknbasaran@gmail.com>
2017-05-04 19:47:05 +0300

    Update db.js

--- confs/db.js
+++ confs/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  test: "mongodb://localhost:27017",
-  prod: "mongodb://u:p@h:port/db"
+  test: "mongodb://127.0.0.1:27017/sau-sozluk-test",
+  prod: "mongodb://127.0.0.1:27017/sau-sozluk-test"
 };
